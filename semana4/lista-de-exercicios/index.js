b63c0e4ab73a0d87471c695fa9ef6573d8b1a386
edupezz <edupezz@gmail.com>
2020-05-11 01:05:57 -0300

    Incremento exercicio

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,62 +1,166 @@
-/*
-Exercício 1:
+// Interpretação de código.
 
-const minhaFuncao = (quantidade) => {
-	const array = []
-	for(let i = 0; i < quantidade; i+=2) {
-			for(let j = 0; j < i; j++) {
-				array.push(j)
-			}
-	}
-        return array
+// 1 - 
+
+/*
+
+O nome da função deixa claro seu objetivo, converter moeda de acordo com a cotação fornecedia
+isso é feito pegando o valor que o usuário fornece já convertida em número através de number()
+a função retornará o parâmetro (valor a ser convertido) multiplicando pela cotação. A chamada
+abaixo fornecer parâmetro 100 para que seja convertido com a cotação previamente fornecida.
+No console, será impresso através da variável meuDinheiro o valor convertido.
+
+Console: 575
+OBS: Cotação de US$ 5,75 obtida hoje 08/05/2020 - antes do fechamento da BOVESPA.
+sujeito a alteração ao fim do dia.
+
+*/
+
+// 2 -
+
+/*
+
+A função tem por objetivo simular investimentos e sua liquidez de acordo com a alíquota
+já fornecida, um seletor de caso (switch case) é usado para que, dependendo do tipo de
+investimento, uma ação seja tomada e um cálculo seja feito, por padrão, se nenhum valor
+forncido na chamada da funçao for atendido o usuário receberá a mensagem "TIPO DE INVES-
+TIMENTO INCORRETO". Duas variáveis são fornecdidas com 2 valores de montantes e investimen
+tos diferentes (novoMontante e segundoMontante, respectivamente) a primeira variável 
+nos dá a opção Ações e o valor de 150 que irá coincidir com a opção Ações do switch case.
+Já a segunda variável nos fornece Tesouro direito e o valor de 200, não temos essa opção
+no switch case, neste caso o usuário recebrá a mensagem default, o padrão.
+
+Console: 
+novoMontante: 165
+segundoMontante: TIPO DE INVESTIMENTO INCORRETO
+
+*/
+
+// 3-
+
+/*
+
+Um for é feito usando a variável numero para pegar os elemento do array numeros, previamene
+iniciado e fornecido, se o valor do índice numero referente ao array numeros for mod 2 =0,
+ou seja: se for divisível por 2 (sobrar resto zero = mod) um array push será realizado para
+incluir valores no araay1, já fornecido e vazio, caso contrário o push será feito no array2
+também fornecido e vazio. No final é solicitado a impressão no console do tamanho dos 3 arrays
+e não seus valores.
+
+Console: 
+numeros.length = 14
+array1.length = 6
+array2.length = 8
+
+*/
+
+// 4-
+
+/*
+
+O loop for irá percorrer os valores do array numeros previamente fornecido e usará a variável
+numero para armazenar seus valores, com isso o IF verificará se o valor sendo avaliado no
+momento é menor que a variável 'numero1' iniciada com o valor infinity, sendo essa condição
+verdadeira a variável numero1 receberá o valor da variável numero, a condição abaixo verifica
+se é  maior, caso positivo a variável número2 receberá o valor de numero.
+
+Console:
+numero1 = 1590
+numero2 = 1590
+
+*/
+//---------------------------------------
+
+
+// Exercícios de lógica de programação:
+
+// 1-
+
+/*
+Podemos usar o For , o For of e o map.
+
+arrayQualquer = [10, 20, 30, 40, 50];
+
+for (Indice of arrayQualquer);
+       if (Indice % 2 ==0){
+         alert("Está feito o exemplo")
+         console.log("está feito o exemplo")
+       }
+
+*/
+
+// 2-
+
+/*
+a) false
+b) false
+c) true
+d) false
+e) true
+
+*/
+
+// 3- 
+
+// não, o código não funcionará.
+
+/*
+
+const quantidadeDeNumerosPares
+let i = 5
+while(i <= quantidadeDeNumerosPares) {
+  console.log(i*2)
 }
 */
-// Respostas:
-// minhafuncao(2) = Array vazio [0]
-// minhafuncao(5) = Array [6]
-// minhafuncao(8) = Array [12]
+
+// 4-
 
 
-//Exercício 2:
+
+// 5- 
+
 /*
-let arrayDeNomes = ["Darvas", "Goli", "João", "Paulinha", "Soter"];
+let num1 = prompt("Digite o primeiro número: ")
+let num2 = prompt("Digite o segundo número: ")
+console.log(`O Primeiro número escolhido foi ${num1} e o segundo foi ${num2}`)
+let maior
+let menor
 
-const funcao = (lista, nome) => {
-  for (let i = 0; i < lista.length; i++) {
-    if (lista[i] === nome) {
-      return i;
-    }
-  }
-};
+if (Number(num1) > Number(num2)){
+    
+  maior = num1
+  menor = num2
 
-console.log(funcao(arrayDeNomes, "Darvas"));
-console.log(funcao(arrayDeNomes, "João"));
-console.log(funcao(arrayDeNomes, "Paula"));
-*/
-// Respostas:
-// As saídas serão: 0,2 e undefined
-// Não funcionariam pois os valores Darvas, João não estariam mais no array, já o valor Paula não está no atual array.
+  console.log(`O maior número é o ${maior} e o menor é o ${menor}`)
 
-// Exercício 3:
-/*
-function metodo(array) {
-        let resultadoA = 0;
-        let resultadoB = 1;
-        let arrayFinal = [];
-      
-        for (let x of array) {
-          resultadoA += x;
-          resultadoB *= x;
-        }
-      
-        arrayFinal.push(resultadoA);
-        arrayFinal.push(resultadoB);
-        return arrayFinal;
-      }
-*/
-// 
-
-function humanToDog (valor){
-        let sum = (valor * 7)
 }
-console.log(humanToDog(4))
+
+if (num1 % num2 == 0) {
+
+  console.log(`O número ${num1} é divisível por ${num2}`)
+
+} else {
+
+  console.log(`o número ${num1} não é divisivel por ${num2}`)
+
+}
+
+if (num2 % num1 == 0) {
+  
+  console.log(`O número ${num2} é divisível por ${num1}`)
+
+} else {
+
+  console.log(`O número ${num2} não é divisível por ${num1}`)
+
+}
+
+*/
+
+
+
+
+
+
+
+
