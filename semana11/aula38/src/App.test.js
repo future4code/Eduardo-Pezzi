89821f5e68940d522e757f936a27d1e3d0cfd5f3
edupezz <edupezz@gmail.com>
2020-06-30 20:41:51 -0300

    Realizando testes

--- semana11/aula38/src/App.test.js
+++ semana11/aula38/src/App.test.js
@@ -1,3 +1,17 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
+import '@testing-library/jest-dom';
 import App from "./App";
+
+
+
+test("Teste conteudo input", async () => {
+    const { getByPlaceholderText } = render(<App />);
+
+        const text = getByPlaceholderText("Novo post");
+
+        fireEvent.change(text, { target: {value: 'teste'}} );
+
+        expect(text).toBe('teste')
+})
+
