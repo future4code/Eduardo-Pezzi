89369b7f21a6b96b460ae0a7c2dd667dce47c05b
edupezz <edupezz@gmail.com>
2020-08-31 19:31:08 -0300

    Incremento exercício tarefa 4 concluída.

--- semana19/aula63/tests/exercicio1e2.test.ts
+++ semana19/aula63/tests/exercicio1e2.test.ts
@@ -74,3 +74,73 @@ test("Testing American Allowed", ()=> {
 
     expect(testResult.americans.allowed).toEqual(["Edward"]);
 });
+
+
+//c)
+
+test("Testing 2 American and 2 Brazilian notAllowed", ()=> {
+    const casinoPlayerBR1: CasinoUser = {
+        name: 'Eduardo',
+        age: 19,
+        nacionality: NACIONALITY.BRAZILIAN
+    };
+
+    const casinoPlayerBR2: CasinoUser = {
+        name: 'Mônica',
+        age: 19,
+        nacionality: NACIONALITY.BRAZILIAN
+    }
+
+    const casinoPlayerUS1: CasinoUser = {
+        name: 'Edward',
+        age: 19,
+        nacionality: NACIONALITY.AMERICAN
+    }
+
+    
+    const casinoPlayerUS2: CasinoUser = {
+        name: 'Sara',
+        age: 19,
+        nacionality: NACIONALITY.AMERICAN
+    }
+
+    const casinoUS: Casino = {
+        name: "manhattan Casino",
+        location: LOCATION.EUA
+    } 
+    const testResult1BR = verifyAge(casinoUS , [casinoPlayerBR1])
+    const testResult2BR = verifyAge(casinoUS , [casinoPlayerBR2])
+    const testResult3US = verifyAge(casinoUS , [casinoPlayerUS1])
+    const testResult4US = verifyAge(casinoUS , [casinoPlayerUS2])
+
+    expect(testResult1BR.brazilians.unallowed).toEqual(["Eduardo"]);
+    expect(testResult2BR.brazilians.unallowed).toEqual(["Mônica"]);
+    expect(testResult3US.americans.unallowed).toEqual(["Edward"]);
+    expect(testResult4US.americans.unallowed).toEqual(["Sara"]);
+});
+
+// d)
+
+test("Allowing two americans and blocking 2 brazilians", () => {
+    const blockedBR: CasinoUser = {
+        name: "Eduardo",
+        age: 19,
+        nacionality: NACIONALITY.BRAZILIAN
+    }
+
+    const allowedUS: CasinoUser = {
+        name: "Edward",
+        age: 21,
+        nacionality: NACIONALITY.AMERICAN
+    }
+
+    const casinoUS: Casino = {
+        name: "Miami Royal Casino",
+        location: LOCATION.EUA
+    }
+
+    const testResult = verifyAge(casinoUS, [blockedBR, blockedBR, allowedUS, allowedUS])
+
+    expect(testResult.brazilians.unallowed).toEqual(["Eduardo", "Eduardo"]);
+    expect(testResult.americans.allowed).toEqual(["Edward", "Edward"]);
+})
