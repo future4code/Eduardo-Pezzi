80a9b9391cd15107f43fd4b8a4fbc689a982f357
edupezz <edupezz@gmail.com>
2020-09-07 01:12:42 -0300

    .

--- semana19/projeto_LAMA/src/routes/bandsRouter.ts
+++ semana19/projeto_LAMA/src/routes/bandsRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express';
+import registerBand from '../controller/registerBand'
 
 export const bandsRouter = express.Router();
 
-bandsRouter.post('/registerband', registerband)
+bandsRouter.post('/registerband', registerBand)
