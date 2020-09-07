981e68a62da3d4934dacbf3b254d2469f7d026d4
edupezz <edupezz@gmail.com>
2020-09-07 01:26:53 -0300

    ..

--- semana19/projeto_LAMA/src/controller/registerBand.ts
+++ semana19/projeto_LAMA/src/controller/registerBand.ts
@@ -5,26 +5,30 @@ import { UserDB } from '../data/UserDB';
 import { BandDB } from '../data/BandDB'
 
 export default async function registerBand(req: Request, res:Response){
-    
-    const token = req.headers.authorization as string
-    const idUser = new Authenticator().getTokenData(token).id
-    const bandName = req.body.bandName;
-    const musicGenre = req.body.musicGenre;
-    const responsible = req.body.responsible;
+    try {
+        const token = req.headers.authorization as string;
+        const idUser = new Authenticator().getTokenData(token).id;
+        const bandName = req.body.bandName;
+        const musicGenre = req.body.musicGenre;
+        const responsible = req.body.responsible;
 
-    const userData = await new UserDB().getUserById(idUser)
-    const role = userData.role;
+        const userData = await new UserDB().getUserById(idUser)
+        const role = userData.role;
 
-    const id = IdGenerator.execute();
+        const id = IdGenerator.execute();
 
-    if(role !== "ADMIN"){
-        res.status(400).send({message: "Not Allowed"})
+        if(role !== "ADMIN"){
+            res.status(400).send({message: "Not Allowed"})
+        }
+
+        if(!bandName || !musicGenre || responsible){
+            res.status(400).send({message: 'Todos os dados de cadastro são necessários'})
+        }
+
+        await new BandDB().registerBand(id, bandName, musicGenre, responsible);
+        res.status(200).send({message: "Banda criada com sucesso!"})
+        
+    } catch (error) {
+        res.status(400).send({message: error.sqlMessage || error.message});
     }
-
-    if(!bandName || !musicGenre || responsible){
-        res.status(400).send({message: 'Todos os dados de cadastro são necessários'})
-    }
-
-    await new BandDB().registerBand(id, bandName, musicGenre, responsible);
-    res.status(200).send({message: "Banda criada com sucesso!"})
 }
--- semana19/projeto_LAMA/src/data/BandDB.ts
+++ semana19/projeto_LAMA/src/data/BandDB.ts
@@ -3,7 +3,8 @@ import { BaseDatabase } from './BaseDB';
 export class BandDB extends BaseDatabase{
     static TABLE_NAME = 'LAMA_BANDAS';
 
-    public async registerBand(id: string, name: string, genre: string, responsible: string){
+    public async registerBand(
+        id: string, name: string, genre: string, responsible: string): Promise<void>{
         await this.getConnection()
         .insert({id, name, genre, responsible})
         .into(BandDB.TABLE_NAME)
